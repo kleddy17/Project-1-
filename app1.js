3bd689f8458050c9472f614eeb41edf6f6146f98
kleddy17 <kleddy17@gmail.com>
2020-10-01 13:05:17 -0400

    next button

--- app1.js
+++ app1.js
@@ -5,10 +5,17 @@ document.addEventListener("DOMContentLoaded", () => {
     const enterIfYouDare = document.querySelector("#btn");
     enterIfYouDare.addEventListener("click", ()=> {
         enterIfYouDare.style.display= "none";
+        const h1 = document.querySelector("h1");
+        h1.style.display="none";
         start();
     });
-        }
-      );
+
+    // const nextButton = document.querySelector("#next-btn");
+    // nextButton.addEventListener("click", getQuestion())
+
+
+}
+);
 
                 /////VARIABLES/////
 
@@ -27,18 +34,18 @@ let score = 0;
 getQuestion();
 }
 
-const nextQuestion = () => {
-    // 1. How to make a click button for next question/add event listener
+// const nextQuestion = () => {
+//     // 1. How to make a click button for next question/add event listener
 
-    // 2. I want to generate new question
-    // getQuestion()
+//     // 2. I want to generate new question
+//     // getQuestion()
     
-    // 3. I want to keep track of what question we are on
-    currentQuestion++
+//     // 3. I want to keep track of what question we are on
+//     currentQuestion++
 
-    // 4. I want to add to the score if there is a right answer
-    // score++
-}
+//     // 4. I want to add to the score if there is a right answer
+//     // score++
+// }
 
 const myQuestions = [
     {   question:"1. What TV show statue lives here?",
@@ -53,33 +60,46 @@ const myQuestions = [
     ];
 
 const getQuestion = () => {
+    
+    //trying to get a random question to show from array
+    //what happens when the next button is clicked
+    //need to splice() every "used" question from array
+    const randomElement = Math.floor(Math.random() * myQuestions.length);
+    console.log(randomElement,myQuestions[randomElement]);
+
+
     const test=document.createElement('section');
     test.setAttribute('id','test');
-    for (let i=0; i<myQuestions.length; i++){
-        const ul=document.createElement('ul');
-        document.body.appendChild(test);
-        test.appendChild(ul);
-        ul.innerHTML=ul.innerHTML + myQuestions[i].question
-        for(let j = 0; j<myQuestions[i].choices.length; j++) {
-            let li=document.createElement('li');
-            ul.appendChild(li);
-            li.innerHTML=li.innerHTML + myQuestions[i].choices[j];
-        }
-    }
+    
+    const questions = document.createElement('ul');
+    //make sure we put under image, append under image
+    document.body.appendChild(test);
+    test.appendChild(questions);
+    questions.innerHTML= `<h1 style="color: pink;", "border: 10px solid white;">${myQuestions[0].question}</h1>`
+
+
+    for(let i = 0; i<myQuestions[0].choices.length; i++) {
+        let li=document.createElement('li');        
+        questions.appendChild(li);
+        li.innerHTML += `<h3 style="color: white;">,<br>${myQuestions[0].choices[i]}</br></h3>`       
+    }     
 }
+
+
 const keepScore = () => {
-const getInputValue = document.querySelectorAll('input[type="text"]');
-    if (getInputValue === myQuestions.answer) {
+    const getInputValue = document.querySelectorAll('input[type="text"]');
+        if (getInputValue === myQuestions.answer) {
         score++
         currentQuestion++
-    }else{ currentQuestion++
-    }
+        }else{ 
+        currentQuestion++
+        }
 }
 
 
 const endGame = () => {
-if (score === 5) {
+    if (score === 5) {
     console.log("You are a powerful witch!")
-}
+    }
 }
 
