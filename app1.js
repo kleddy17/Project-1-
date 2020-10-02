3df0855a1b6c0322f9377e129d163f67da9ce2dd
kleddy17 <kleddy17@gmail.com>
2020-10-02 14:28:38 -0400

    working on the score

--- app1.js
+++ app1.js
@@ -11,9 +11,17 @@ document.addEventListener("DOMContentLoaded", () => {
     });
     const nextButton = document.querySelector("#next-btn");
     nextButton.addEventListener("click", () => {
+        const removeQuesAns = document.querySelector("#test")
+        removeQuesAns.style.display="none";
+        // keepScore();
         getQuestion();
-        keepScore();
-    })
+    });
+
+    // const submitButton = document.querySelector("#userInput")
+    // submitButton.addEventListener("click", () => {
+    //     // getQuestion();
+    //     keepScore();
+    // })
 }
 );
 
@@ -21,6 +29,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
 let score 
 let currentQuestion 
+let questionAsk
 let totalQuestion 
 let rightAnswer
 
@@ -32,7 +41,7 @@ const start = () => {
 let currentQuestion = 0;
 let score = 0;
 getQuestion();
-keepScore();
+getInputValue();
 }
 
 // const nextQuestion = () => {
@@ -51,50 +60,67 @@ keepScore();
 const myQuestions = [
     {   question:"What TV show statue lives here?",
         choices:["Bewitched", "Elvira", "Sabrina, The Teenage Witch"],
-        answer:0,
+        answer:"Bewitched",
         
     },
     {   question:"In what year did the Salem Witch Trials take place?",
         choices:["1692", "1776", "1806"],
-        answer:"1692"
-    }
+        answer:"1692",
+    },
+    {   question: "Which accused person was pressed to death for refusing to answer the court's questions?",
+        choices:["Giles Corey", "Tituba","Samuel Wardwell"],
+        answer:"Giles Corey",
+    },
+
+    {   question:"How many convicted witches were hanged?",
+        choices:["19", "107", "45", "20"],
+        answer: "19"
+    },
+
+    {   question:"Members of which Salem Village family did much of the accusing?",
+        choices: ["Porter","Putnam","Ryan"],
+        answer: "Putnam"
+    },
+
+
+
     ];
 
 const getQuestion = () => {
-    let randomElement = Math.floor(Math.random() * myQuestions.length);
-    
-    let currentQuestion = myQuestions.splice(randomElement, 1)
+    // Making the function choose a random question, then using splice to 
+    // take used question from the array. 
+let randomElement = Math.floor(Math.random() * myQuestions.length);
+let currentQuestion = myQuestions.splice(randomElement, 1)
     console.log("currentQuestion:", currentQuestion);
     console.log("currentQuestion:", currentQuestion[0])
     console.log(randomElement)
 
-    const test=document.createElement('section');
-    test.setAttribute('id','test');
+const test=document.createElement('section');
+test.setAttribute('id','test');
     
-    const questions = document.createElement('ul');
-    //make sure we put under image, append under image
-    document.body.appendChild(test);
-    test.appendChild(questions);
-    questions.innerHTML += `<h1 class="ques">${currentQuestion[0].question}</h1>`
-        for(let i = 0; i<currentQuestion[0].choices.length; i++) {
-            let li=document.createElement('li'); 
-            li.innerHTML = `<h3 class="choice">,<br>${currentQuestion[0].choices[i]}</br></h3>`       
-            questions.appendChild(li);
+const questions = document.createElement('ul');
+    // Make sure we put under image, append under image.
+document.body.appendChild(test);
+test.appendChild(questions);
+questions.innerHTML += `<h1 class="ques">${currentQuestion[0].question}</h1>`
+    for(let i = 0; i<currentQuestion[0].choices.length; i++) {
+        let li=document.createElement('li');
+        li.innerHTML = `<h3 class="option">, <br>${currentQuestion[0].choices[i]}</br></h3>`
+        questions.appendChild(li);
+            
     } 
 }
 
 
-const keepScore = () => {
-    let score = 0;
-    const getInputValue = document.querySelectorAll("#compareAnswer");
-    console.log(getInputValue);
-        if (getInputValue === myQuestions.answer) {
-            score++
-            // currentQuestion++
-        }else{ 
-            // currentQuestion++
-     }
-console.log(score)
+const getInputValue = () => {
+let questionAsk = 0;
+let score = 0;
+const userInput = document.querySelector("#myInput").value;
+    if (userInput === myQuestions.answer){
+        score++
+        questionAsk++
+    }
+
 }
 
 
@@ -102,7 +128,7 @@ console.log(score)
 
 const endGame = () => {
     if (score === 5) {
-    console.log("You are a powerful witch!")
+        console.log("You are a powerful witch!")
     }
 }
 
